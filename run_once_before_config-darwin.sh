c82d7f5e5bed573a9eb2aa65c0447409f6fcbe34
Juan Andres Moreno <juan.andres.208@hotmail.com>
2022-11-15 19:51:59 -0500

    Add files via upload

--- run_once_before_config-darwin.sh
+++ run_once_before_config-darwin.sh
@@ -12,3 +12,18 @@ defaults write -g com.apple.swipescrolldirection -bool FALSE
 
 # Autohide dock
 osascript -e "tell application \"System Events\" to set the autohide of the dock preferences to true"
+
+#Improve dock speed
+defaults write com.apple.dock autohide-time-modifier -float 0.15
+
+
+# config hot corners
+## Botton-left show apps
+defaults write com.apple.dock wvous-bl-corner -int 11
+
+## Botton-right show desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+
+
+
+killall Dock
