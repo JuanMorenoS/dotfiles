ec35c60e14be737be19d06ebd06f507135464467
Juan Andres Moreno <juan.andres.208@hotmail.com>
2025-04-15 09:02:22 -0500

    Update run_once_after_nix.sh

--- run_once_after_nix.sh
+++ run_once_after_nix.sh
@@ -9,7 +9,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # sudo chmod 644 /etc/synthetic.conf
 curl -L https://nixos.org/nix/install | sh -s -- --daemon
 
-nix --extra-experimental-features flakes --extra-experimental-features nix-command run nix-darwin -- switch --flake ~/.config/nix
+nix --extra-experimental-features flakes --extra-experimental-features nix-command run nix-darwin -- switch --flake ~/.config/nix#Juans-MacBook-Pro
+
 
 # Throbleshoting, Maybe you need to run this command
 # sudo ln -s /nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt.before-nix-darwin
